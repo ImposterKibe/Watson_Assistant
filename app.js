942ff3db5fa79332d071b51b1cfb50d12c89dacf
ImposterKibe <kibestevemwa@gmail.com>
2018-09-20 14:39:13 +0300

    Added watson tone analyzer functionality

--- app.js
+++ app.js
@@ -7,6 +7,45 @@ require('dotenv').config()
 const prompt = require('prompt')
 const AssistantV1 = require('watson-developer-cloud/assistant/v1')
 
+// Set up Assistant service wrapper.
+const service = new AssistantV1({
+  username: process.env.ASSISTANT_USERNAME, // replace with service username
+  password: process.env.ASSISTANT_PASSWORD, // replace with service password
+  version: '2018-09-18'
+});
+
+const workspace_id = process.env.WORKSPACE_ID; // replace with workspace ID
+
+//Watson Tone Analyzer
+const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3')
+const tone_analyzer= new ToneAnalyzerV3({
+  username: process.env.TONE_USERNAME, // replace with service username
+  password: process.env.TONE_PASSWORD, // replace with service password
+  version: 'V1',
+  version_date:'2016-05-19',
+  url: process.env.TONE_URL
+})
+
+const my_text='Hi, partner'
+
+const toneParams= {
+  'tone_input':{'text':my_text},
+  'content_type':'application/json'
+}
+
+tone_analyzer.tone(toneParams,(err,result)=>{
+  if(err){
+    return console.log(err)
+  }
+  const cats = result.document_tone.tone_categories;
+  cats.forEach((cat)=>{
+    console.log('\n'+cat.category_name)
+    cat.tones.forEach((tone)=>{
+      console.log("%s: %s",tone.tone_name, tone.score)
+    })
+  })
+})
+
 //Swagger server dependencies
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
@@ -29,16 +68,6 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
     console.log('Server Running.....');
   }
 });
-
-
-// Set up Assistant service wrapper.
-const service = new AssistantV1({
-  username: process.env.ASSISTANT_USERNAME, // replace with service username
-  password: process.env.ASSISTANT_PASSWORD, // replace with service password
-  version: '2018-09-18'
-});
-
-const workspace_id = process.env.WORKSPACE_ID; // replace with workspace ID
 
 // Start conversation with empty message.
 service.message({

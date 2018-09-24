e5e541ce8a8bf6f60e222b92a2758571eb9deb91
ImposterKibe <kibestevemwa@gmail.com>
2018-09-24 14:35:20 +0300

    Added watson natural language understander

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 
 // Load Environment Variable
 require('dotenv').config()
+const Tweet = require('twit')
 
-//Watson Assistant dependencies
 //Swagger server dependencies
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
@@ -13,12 +13,41 @@ module.exports = app; // for testing
 var config = {
   appRoot: __dirname // required config
 };
-
+//Watson Assistant dependencies
 const prompt = require('prompt')
 const AssistantV1 = require('watson-developer-cloud/assistant/v1')
 const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3')
 // const speechToTextV1 = require('watson-developer-cloud/text-to-speech/v1')
 const language_translatorV3 = require('watson-developer-cloud/language-translator/v3')
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1')
+
+const watson_lang_analyzer = new NaturalLanguageUnderstandingV1({
+  version: '2018-05-19',
+  username: process.env.LANGUAGE_UNDERSTANDER_USERNAME,
+  password: process.env.LANGUAGE_UNDERSTANDER_PASSWORD,
+  url:process.env.LANGUAGE_UNDERSTANDER_URL
+})
+
+var lang_parameters = {
+  'text': 'IBM is an American multinational technology company headquartered in Armonk, New York, United States, with operations in over 170 countries.',
+  'features': {
+    'entities': {
+      'emotion': true,
+      'sentiment': true,
+      'limit': 2
+    },
+    'keywords': {
+      'emotion': true,
+      'sentiment': true,
+      'limit': 2
+    }
+  }
+}
+watson_lang_analyzer.analyze(lang_parameters, langData)
+
+function langData (err,result){
+  console.log(result)
+}
 const workspace_id = process.env.WORKSPACE_ID; // replace with workspace ID
 
 // Set up Speech-to-Text service wrapper
@@ -29,22 +58,6 @@ const speechToText= new speechToTextV1({
   url:process.env.SPEECH_URL
 })
 */
-
-//Set Up Translator service
-const language_translator= new language_translatorV3({
-  username: process.env.TRANSLATOR_USERNAME ,
-  password: process.env.TRANSLATOR_PASSWORD,
-  url:process.env.TRANSLATOR_URL,
-  version: '2018-09-20'
-})
-
-// Set up Assistant service wrapper.
-const assistant = new AssistantV1({
-  username: process.env.ASSISTANT_USERNAME, // replace with service username
-  password: process.env.ASSISTANT_PASSWORD, // replace with service password
-  version: '2018-09-18'
-});
-
 //Watson Tone Analyzer Service wrapper
 const tone_analyzer= new ToneAnalyzerV3({
   username: process.env.TONE_USERNAME, // replace with service username
@@ -55,31 +68,61 @@ const tone_analyzer= new ToneAnalyzerV3({
 })
 
 
-const my_text= 'Hi, partner'
-
 //Parameters
-const toneParams= {
-  'tone_input':{'text':my_text},
-  'content_type':'application/json'
+
+
+//Twitter service wrapper
+const twit = new Tweet({
+  consumer_key:process.env.TWITTER_API_KEY,
+  consumer_secret:process.env.TWITTER_SECRET_KEY,
+  access_token:process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret:process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
+
+//tweet Search Parameters
+const tweetParams ={
+  q:'Eminem',
+  count: 1
 }
-const translatorParams= {
+
+//twit.get('search/tweets',tweetParams,gotData)
+
+function gotData (err,data,response)      {
+  const tweets = data.statuses
+  for (let i = 0; i<tweets.length;i++){
+    const my_text= tweets[i].text
+    console.log(tweets[i].text)
+    const toneParams= {
+      'tone_input':{'text':my_text},
+      'content_type':'application/json'
+    }
+    //Tone Analyser instance
+    tone_analyzer.tone(toneParams,(err,result)=>{
+      if(err){
+        return console.log(err)
+      }
+    const cats = result.document_tone.tone_categories;
+    cats.forEach((cat)=>{
+      console.log('\n'+cat.category_name)
+      cat.tones.forEach((tone)=>{
+        console.log("%s: %s",tone.tone_name, tone.score)
+      })
+    })
+  })
+  }
+}
+
+//Set Up Translator service
+const language_translator= new language_translatorV3({
+  username: process.env.TRANSLATOR_USERNAME ,
+  password: process.env.TRANSLATOR_PASSWORD,
+  url:process.env.TRANSLATOR_URL,
+  version: '2018-09-20'
+})
+/*const translatorParams= {
   text: 'Hello',
   model_id: 'en-es'
 }
-/*
-//Tone Analyser instance
-tone_analyzer.tone(toneParams,(err,result)=>{
-  if(err){
-    return console.log(err)
-  }
-  const cats = result.document_tone.tone_categories;
-  cats.forEach((cat)=>{
-    console.log('\n'+cat.category_name)
-    cat.tones.forEach((tone)=>{
-      console.log("%s: %s",tone.tone_name, tone.score)
-    })
-  })
-})
 //Translator instance
 language_translator.translate(translatorParams,(err,result)=>{
   if(err){
@@ -88,6 +131,14 @@ language_translator.translate(translatorParams,(err,result)=>{
   console.log(result)
 })
 */
+// Set up Assistant service wrapper.
+const assistant = new AssistantV1({
+  username: process.env.ASSISTANT_USERNAME, // replace with service username
+  password: process.env.ASSISTANT_PASSWORD, // replace with service password
+  version: '2018-09-18'
+});
+
+
 
 //Server
 SwaggerExpress.create(config, function(err, swaggerExpress) {
